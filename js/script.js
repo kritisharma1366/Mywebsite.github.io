475cc07284f33108ca775cfc6248b56a12e33a34
Kriti Sharma <87195340+kritisharma136@users.noreply.github.com>
2022-08-10 15:54:40 +0530

    portfolio website built using html,css and,js

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Web Developer","Software Developer"],
+    typeSpeed:100,
+    BackSpeed:100,
+    loop:true
+
+})
+
+const nav=document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList=navList.length;
+    allSection=document.querySelectorAll(".section"),
+    toalSection=allSection.length;
+    for(let i=0;i<totalNavList;i++){
+        const a=navList[i].querySelector("a");
+        a.addEventListener("click",function()
+        {
+            for(let j=0;j<totalNavList;j++){
+                navList[j].querySelector("a").classList.remove("active");
+
+            }
+            this.classList.add("active")
+            showSection(this);
+        })
+    }
+    function showSection(element){
+        for(let i=0;i<totalSection;i++){
+            allSection[i].classList.remove("active");
+        }
+        const target=element.getAttribute("href").split("#")[1];
+        document.querySelector("#"+target).classList.add("active")
+
+    }
---
+++ js/style-switcher.js
@@ -0,0 +1,31 @@
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", ()=>{
+    document.querySelector(".style-switcher").classList.toggle("open") 
+})
+
+window.addEventListener("scroll",()=>{
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+
+/*=======================theme-colore==========*/
+
+
+
+const alternateStyles=document.querySelectorAll(".alternate-style");
+function setActiveStyle(color)
+{
+    alternateStyles.forEach((style)=>{
+        if(color===style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+
+        }
+        else{
+            style.setAttribute ("disabled","true")
+        }
+    }) 
+}
+
